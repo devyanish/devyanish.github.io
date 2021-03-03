0a0d92bbbb4ddf55b71c1123947aa53734819278
devyanish <69156586+devyanish@users.noreply.github.com>
2021-03-03 10:18:49 +0000

    Login/Register

---
+++ fireBase.js
@@ -0,0 +1,14 @@
+var fireBase = fireBase || firebase;
+var firebaseConfig = {
+    apiKey: "AIzaSyBl0dFey4LafiQEl-OG7vvw5doa0MV-mII",
+    authDomain: "voom-firebase-auth.firebaseapp.com",
+    projectId: "voom-firebase-auth",
+    storageBucket: "voom-firebase-auth.appspot.com",
+    messagingSenderId: "348510789420",
+    appId: "1:348510789420:web:7c080c441e98dcbe0dd689",
+    measurementId: "G-V2Z3J17C4W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
